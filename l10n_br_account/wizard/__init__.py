3401df1d9004111ff3e1c4fd0f22008bb9156e27
Vinicius Dittgen <vinipd@gmail.com>
2012-04-25 15:58:53 -0300

    fix license.

--- l10n_br_account/wizard/__init__.py
+++ l10n_br_account/wizard/__init__.py
@@ -14,7 +14,7 @@
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
 #GNU Affero General Public License for more details.                            #
 #                                                                               #
-#You should have received a copy of the GNU General Public License              #
+#You should have received a copy of the GNU Affero General Public License       #
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
 #################################################################################
 import l10n_br_account_nfe_export

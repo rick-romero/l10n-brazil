8083ee11d49eeea3e43e7b0a437a1b23222fb033
Vinicius Dittgen <vinipd@gmail.com>
2012-06-21 14:48:22 -0300

    fix test_product_sale_to_invoice for oerp version 6.1

--- tests_use_case/test_product_sale_to_invoice.py
+++ tests_use_case/test_product_sale_to_invoice.py
@@ -6,12 +6,18 @@ def test_product_sale_to_invoice_nfe(oerp):
     document.
         taxes in this invoice are:
             COFINS 0,74% Incluso
+            PIS 0,25% Incluso
             CSLL 0,21% Incluso
             INSS 1,80% Incluso
             ICMS 1,25% Incluso
             IPI 5,00% Adicionado
+    Confs required:
+         Brazilian chart of accounts generated;
+         Fiscal document serie for NFE fiscal document;
+         CNAE da empresa.
 
     """
+
     #create empty fiscal classification for the product
     prod_fc_obj = oerp.pool.get('account.product.fiscal.classification')
     prod_fc_id = prod_fc_obj.create(oerp.cr, 1, {
@@ -20,42 +26,7 @@ def test_product_sale_to_invoice_nfe(oerp):
         })
 
     assert prod_fc_obj.browse(oerp.cr, 1, [prod_fc_id])[0].id == prod_fc_id
-
-    # create product fiscal operation category
-    cfop_obj = oerp.pool.get('l10n_br_account.cfop')
-    cfop_id = cfop_obj.search(oerp.cr, 1, [])[0]
-    fiscal_doc_obj = oerp.pool.get('l10n_br_account.fiscal.document')
-    fiscal_doc_id = fiscal_doc_obj.search(oerp.cr, 1, [('nfe', '=', True)])[0]
-
-    fopc_obj = oerp.pool.get('l10n_br_account.fiscal.operation.category')
-    fopc_id = fopc_obj.create(oerp.cr, 1, {
-        'code': 'produtos',
-        'name': 'produtos',
-        'type': 'output',
-        'use_sale': True,
-        'use_invoice': True,
-        'fiscal_type': 'product'
-        })
-
-    assert fopc_obj.browse(oerp.cr, 1, [fopc_id])[0].id == fopc_id
-
-    # create product fiscal operation
-    fop_obj = oerp.pool.get('l10n_br_account.fiscal.operation')
-    fop_id = fop_obj.create(oerp.cr, 1, {
-        'code': 'produto01',
-        'name': 'produto01',
-        'type': 'output',
-        'fiscal_operation_category_id': fopc_id,
-        'cfop_id': cfop_id,
-        'fiscal_document_id': fiscal_doc_id,
-        'use_sale': True,
-        'use_invoice': True,
-        'fiscal_type': 'product'
-        })
-
-    assert fop_obj.browse(oerp.cr, 1, [fop_id])[0].id == fop_id
-
-    # create product template
+    # create product template - utiliza plano de contas brasileiro
     prod_obj = oerp.pool.get('product.product')
     prod_tpl_obj = oerp.pool.get('product.template')
 
@@ -64,7 +35,10 @@ def test_product_sale_to_invoice_nfe(oerp):
         'standard_price': 1.00,
         'list_price': 100.00,
         'mes_type': 'fixed',
-        'name': 'product test',
+        'name': 'pytest product',
+        'property_account_income': 1193,
+        'property_account_expense': 669,
+        'property_fiscal_classification': prod_fc_id,
         'uom_po_id': 1,
         'type': 'product',
         'procure_method': 'make_to_stock',
@@ -78,7 +52,7 @@ def test_product_sale_to_invoice_nfe(oerp):
     prod_id = prod_obj.create(oerp.cr, 1, {
         'product_tmpl_id': prod_tpl_id,
         'valuation': 'manual_periodic',
-        'default_code': '123'
+        'default_code': 'pytest_123'
         })
 
     assert prod_obj.browse(oerp.cr, 1, [prod_id])[0].id == prod_id
@@ -92,13 +66,23 @@ def test_product_sale_to_invoice_nfe(oerp):
         'tax_discount': True,
         'tax_include': True,
         'notprintable': True,
-        'domain': 'cofins_test'
+        'domain': 'cofins'
+        })
+
+    # create PIS tax code with tax_include
+    tax_code_pis_id = tax_code_obj.create(oerp.cr, 1, {
+        'name': 'PIS_test',
+        'company_id': 1,
+        'sign': 1,
+        'tax_discount': True,
+        'tax_include': True,
+        'notprintable': True,
+        'domain': 'pis'
         })
 
     assert tax_code_obj.browse(oerp.cr, 1, [tax_code_cofins_id])[0].id == tax_code_cofins_id
 
     # create CSLL tax code with tax_include
-    tax_code_obj = oerp.pool.get('account.tax.code')
     tax_code_csll_id = tax_code_obj.create(oerp.cr, 1, {
         'name': 'CSLL_test',
         'company_id': 1,
@@ -106,13 +90,12 @@ def test_product_sale_to_invoice_nfe(oerp):
         'tax_discount': True,
         'tax_include': True,
         'notprintable': True,
-        'domain': 'csll_test'
+        'domain': 'csll'
         })
 
     assert tax_code_obj.browse(oerp.cr, 1, [tax_code_csll_id])[0].id == tax_code_csll_id
 
     # create INSS tax code with tax_include
-    tax_code_obj = oerp.pool.get('account.tax.code')
     tax_code_inss_id = tax_code_obj.create(oerp.cr, 1, {
         'name': 'INSS_test',
         'company_id': 1,
@@ -120,13 +103,12 @@ def test_product_sale_to_invoice_nfe(oerp):
         'tax_discount': True,
         'tax_include': True,
         'notprintable': True,
-        'domain': 'inss_test'
+        'domain': 'inss'
         })
 
     assert tax_code_obj.browse(oerp.cr, 1, [tax_code_inss_id])[0].id == tax_code_inss_id
 
     # create ICMS tax code with tax_include
-    tax_code_obj = oerp.pool.get('account.tax.code')
     tax_code_icms_id = tax_code_obj.create(oerp.cr, 1, {
         'name': 'ICMS_test',
         'company_id': 1,
@@ -134,13 +116,12 @@ def test_product_sale_to_invoice_nfe(oerp):
         'tax_discount': True,
         'tax_include': True,
         'notprintable': False,
-        'domain': 'icms_test'
+        'domain': 'icms'
         })
 
     assert tax_code_obj.browse(oerp.cr, 1, [tax_code_icms_id])[0].id == tax_code_icms_id
 
     # create IPI tax code with tax_include
-    tax_code_obj = oerp.pool.get('account.tax.code')
     tax_code_ipi_id = tax_code_obj.create(oerp.cr, 1, {
         'name': 'IPI_test',
         'company_id': 1,
@@ -148,7 +129,7 @@ def test_product_sale_to_invoice_nfe(oerp):
         'tax_discount': True,
         'tax_include': False,
         'notprintable': False,
-        'domain': 'ipi_test'
+        'domain': 'ipi'
         })
 
     assert tax_code_obj.browse(oerp.cr, 1, [tax_code_icms_id])[0].id == tax_code_icms_id
@@ -173,14 +154,13 @@ def test_product_sale_to_invoice_nfe(oerp):
         'tax_add': False,
         'tax_include': True,
         'tax_retain': False,
-        'domain': 'csll_test',
+        'domain': 'csll',
         })
 
     assert tax_obj.browse(oerp.cr, 1, [tax_csll_id])[0].id == tax_csll_id
     sale_order_taxes.append(tax_csll_id)
 
     # create COFINS 0,74% tax with type tax_include
-    tax_obj = oerp.pool.get('account.tax')
     tax_cofins_id = tax_obj.create(oerp.cr, 1, {
         'sequence': '1',
         'type_tax_use': 'all',
@@ -197,14 +177,36 @@ def test_product_sale_to_invoice_nfe(oerp):
         'tax_add': False,
         'tax_include': True,
         'tax_retain': False,
-        'domain': 'cofins_test',
+        'domain': 'cofins',
         })
 
     assert tax_obj.browse(oerp.cr, 1, [tax_cofins_id])[0].id == tax_cofins_id
     sale_order_taxes.append(tax_cofins_id)
 
+    # create PIS 0,25% tax with type tax_include
+    tax_pis_id = tax_obj.create(oerp.cr, 1, {
+        'sequence': '1',
+        'type_tax_use': 'all',
+        'applicable_type': 'true',
+        'company_id': 1,
+        'name': 'PIS 0,25% Recolhe_test',
+        'amount': 0.0025,
+        'type': 'percent',
+        'tax_code_id': tax_code_pis_id,
+        'base_reduction': 0.0000,
+        'amount_mva': 0.0000,
+        'price_include': False,
+        'tax_discount': True,
+        'tax_add': False,
+        'tax_include': True,
+        'tax_retain': False,
+        'domain': 'pis',
+        })
+
+    assert tax_obj.browse(oerp.cr, 1, [tax_pis_id])[0].id == tax_pis_id
+    sale_order_taxes.append(tax_pis_id)
+
     # create INSS 1,80% tax with type tax_include
-    tax_obj = oerp.pool.get('account.tax')
     tax_inss_id = tax_obj.create(oerp.cr, 1, {
         'sequence': '1',
         'type_tax_use': 'all',
@@ -221,14 +223,13 @@ def test_product_sale_to_invoice_nfe(oerp):
         'tax_add': False,
         'tax_include': True,
         'tax_retain': False,
-        'domain': 'inss_test',
+        'domain': 'inss',
         })
 
     assert tax_obj.browse(oerp.cr, 1, [tax_inss_id])[0].id == tax_inss_id
     sale_order_taxes.append(tax_inss_id)
 
     # create ICMS 1,25% tax with type tax_include
-    tax_obj = oerp.pool.get('account.tax')
     tax_icms_id = tax_obj.create(oerp.cr, 1, {
         'sequence': '1',
         'type_tax_use': 'all',
@@ -245,14 +246,13 @@ def test_product_sale_to_invoice_nfe(oerp):
         'tax_add': False,
         'tax_include': True,
         'tax_retain': False,
-        'domain': 'icms_test',
+        'domain': 'icms',
         })
 
     assert tax_obj.browse(oerp.cr, 1, [tax_icms_id])[0].id == tax_icms_id
     sale_order_taxes.append(tax_icms_id)
 
     # create IPI 5% tax with type tax_include
-    tax_obj = oerp.pool.get('account.tax')
     tax_ipi_id = tax_obj.create(oerp.cr, 1, {
         'sequence': '1',
         'type_tax_use': 'all',
@@ -269,21 +269,149 @@ def test_product_sale_to_invoice_nfe(oerp):
         'tax_add': True,
         'tax_include': False,
         'tax_retain': False,
-        'domain': 'ipi_test',
+        'domain': 'ipi',
         })
 
     assert tax_obj.browse(oerp.cr, 1, [tax_ipi_id])[0].id == tax_ipi_id
     sale_order_taxes.append(tax_ipi_id)
 
+    #create CSTs
+    cst_obj = oerp.pool.get('l10n_br_account.cst')
+    cst_icms_id = cst_obj.create(oerp.cr, 1, {
+        'name': 'isento',
+        'code': 40,
+        'tax_code_id': tax_code_icms_id
+        })
+    assert cst_obj.browse(oerp.cr, 1, [cst_icms_id])[0].id == cst_icms_id
+    
+    cst_cofins_id = cst_obj.create(oerp.cr, 1, {
+        'name': 'isento',
+        'code': 07,
+        'tax_code_id': tax_code_cofins_id
+        })
+    assert cst_obj.browse(oerp.cr, 1, [cst_cofins_id])[0].id == cst_cofins_id
+    
+    cst_ipi_id = cst_obj.create(oerp.cr, 1, {
+        'name': 'isento',
+        'code': 02,
+        'tax_code_id': tax_code_ipi_id
+        })
+    assert cst_obj.browse(oerp.cr, 1, [cst_ipi_id])[0].id == cst_ipi_id
+    
+    cst_pis_id = cst_obj.create(oerp.cr, 1, {
+        'name': 'isento',
+        'code': 07,
+        'tax_code_id': tax_code_pis_id
+        })
+    assert cst_obj.browse(oerp.cr, 1, [cst_pis_id])[0].id == cst_pis_id
+
+    # create account journal
+    acc_journal_obj = oerp.pool.get('account.journal')
+    acc_journal_id = acc_journal_obj.create(oerp.cr, 1, {
+        'code': 'bco',
+        'name': 'bco',
+        'view_id': 3,
+        'sequence_id': 1,
+        'company_id': 1,
+        'revenue_expense': True,
+        'type': 'cash'
+        })
+
+    assert acc_journal_obj.browse(oerp.cr, 1, [acc_journal_id])[0].id == acc_journal_id
+
+    # create product fiscal operation category
+    cfop_obj = oerp.pool.get('l10n_br_account.cfop')
+    cfop_id = cfop_obj.search(oerp.cr, 1, [])[0]
+    fiscal_doc_obj = oerp.pool.get('l10n_br_account.fiscal.document')
+    fiscal_doc_id = fiscal_doc_obj.search(oerp.cr, 1, [('nfe', '=', True)])[0]
+
+    fopc_obj = oerp.pool.get('l10n_br_account.fiscal.operation.category')
+    fopc_id = fopc_obj.create(oerp.cr, 1, {
+        'code': 'pytest_produtos',
+        'name': 'pytest_produtos',
+        'type': 'output',
+        'use_sale': True,
+        'use_invoice': True,
+        'fiscal_type': 'product'
+        })
+
+    assert fopc_obj.browse(oerp.cr, 1, [fopc_id])[0].id == fopc_id
+
+    oerp.cr.execute('insert into l10n_br_account_fiscal_operation_category_rel (fiscal_operation_category_id,journal_id) values (%s, %s)' % (fopc_id, acc_journal_id))
+
+    # create product fiscal operation
+    fop_obj = oerp.pool.get('l10n_br_account.fiscal.operation')
+
+    fop_id = fop_obj.create(oerp.cr, 1, {
+        'code': 'pytest_produto01',
+        'name': 'pytest_produto01',
+        'type': 'output',
+        'fiscal_operation_category_id': fopc_id,
+        'cfop_id': cfop_id,
+        'fiscal_document_id': fiscal_doc_id,
+        'use_sale': True,
+        'use_invoice': True,
+        'fiscal_type': 'product'
+        })
+
+    assert fop_obj.browse(oerp.cr, 1, [fop_id])[0].id == fop_id
+
+    fop_line_obj = oerp.pool.get('l10n_br_account.fiscal.operation.line')
+
+    fop_lines = []
+
+    fopl1 = {'company_id': 1,
+            'fiscal_classification_id': prod_fc_id,
+            'tax_code_id': tax_code_icms_id,
+            'cst_id': cst_icms_id,
+            'fiscal_operation_id': fop_id
+            }
+
+    fopl2 = {'company_id': 1,
+            'fiscal_classification_id': prod_fc_id,
+            'tax_code_id': tax_code_cofins_id,
+            'cst_id': cst_cofins_id,
+            'fiscal_operation_id': fop_id
+            }
+
+    fopl3 = {'company_id': 1,
+            'fiscal_classification_id': prod_fc_id,
+            'tax_code_id': tax_code_ipi_id,
+            'cst_id': cst_ipi_id,
+            'fiscal_operation_id': fop_id
+            }
+
+    fopl4 = {'company_id': 1,
+            'fiscal_classification_id': prod_fc_id,
+            'tax_code_id': tax_code_pis_id,
+            'cst_id': cst_pis_id,
+            'fiscal_operation_id': fop_id
+            }
+
+    fop_lines.append((0, 0, fopl1))
+    fop_lines.append((0, 0, fopl2))
+    fop_lines.append((0, 0, fopl3))
+    fop_lines.append((0, 0, fopl4))
+
+    fop_obj.write(oerp.cr, 1, fop_id, {
+        'fiscal_operation_line': fop_lines
+        })
+
     # update company data
     partner_obj = oerp.pool.get('res.partner')
     partner_address_obj = oerp.pool.get('res.partner.address')
+    country_obj = oerp.pool.get('res.country')
+    state_obj = oerp.pool.get('res.country.state')
+
+    country_id = country_obj.search(oerp.cr, 1, [('code', '=', 'BR')])[0]
+    state_id = state_obj.search(oerp.cr, 1, [('code', '=', 'RS'),('country_id', '=', country_id)])[0]
 
     partner_obj.write(oerp.cr, 1, 1, {
         'name': 'emp teste',
         'tipo_pessoa': 'J',
         'legal_name': 'emp teste',
         'cnpj_cpf': '10.965.392/0001-87',
+        'inscr_est': '1111',
         'partner_fiscal_type_id': 1
         })
 
@@ -294,10 +422,10 @@ def test_product_sale_to_invoice_nfe(oerp):
         'partner_id': 1,
         'city': 'poa',
         'name': 'Vinicius',
-        'country_id': 29,
+        'country_id': country_id,
         'type': 'default',
         'email': 'proge@proge.com.br',
-        'state_id': 1,
+        'state_id': state_id,
         'l10n_br_city_id': 4530,
         'number': 34,
         'district': 'abcd',
@@ -323,10 +451,10 @@ def test_product_sale_to_invoice_nfe(oerp):
         'partner_id': partner_client_id,
         'city': 'poa',
         'name': 'Carlos',
-        'country_id': 29,
+        'country_id': country_id,
         'type': 'default',
         'email': 'proge@proge.com.br',
-        'state_id': 1,
+        'state_id': state_id,
         'l10n_br_city_id': 4530,
         'number': 34,
         'district': 'abcd',
@@ -347,7 +475,9 @@ def test_product_sale_to_invoice_nfe(oerp):
             'product_uom_qty': 1,
             'product_id': prod.id,
             'product_uom': 1,
-            'price_unit': prod.price_get('list_price')[prod.id]
+            'price_unit': prod.price_get('list_price')[prod.id],
+            'fiscal_operation_category_id': fopc_id,
+            'fiscal_operation_id': fop_id
             }
 
     sale_order_lines.append((0, 0, sol))
@@ -361,18 +491,21 @@ def test_product_sale_to_invoice_nfe(oerp):
         'pricelist_id': 1,
         'order_line': sale_order_lines,
         'fiscal_operation_category_id': fopc_id,
-        'fiscal_operation_id': fop_id
+        'fiscal_operation_id': fop_id,
+        'fiscal_classification_id': prod_fc_id
         #'fiscal_position': 1
         })
 
     assert sale_order_obj.browse(oerp.cr, 1, [order_id])[0].id == order_id
 
     sol_id = sale_order_line_obj.search(oerp.cr, 1, [('order_id', '=', order_id)])[0]
+    
+    assert sale_order_line_obj.browse(oerp.cr, 1, [sol_id])[0].fiscal_operation_id.id == fop_id
 
     for tax_id in sale_order_taxes:
         oerp.cr.execute('insert into sale_order_tax (order_line_id,tax_id) values (%d,%d)' % (sol_id, int(tax_id)))
 
-    assert len(sale_order_obj.browse(oerp.cr, 1, [order_id])[0].order_line[0].tax_id) == 5
+    assert len(sale_order_obj.browse(oerp.cr, 1, [order_id])[0].order_line[0].tax_id) == 6
 
     sale_order_name = sale_order_obj.browse(oerp.cr, 1, [order_id])[0].name
 
@@ -390,23 +523,23 @@ def test_product_sale_to_invoice_nfe(oerp):
 
     assert invoice.origin == sale_order_name
     inv_taxes = invoice.invoice_line[0].invoice_line_tax_id
-    assert len(inv_taxes) == 5
+    assert len(inv_taxes) == 6
     assert invoice.amount_total == 105.0
     assert invoice.amount_untaxed == 100.0
     assert invoice.amount_tax == 5.00
-    assert invoice.cfop_id.id == cfop_id
+    #assert invoice.cfop_id.id == cfop_id
     assert invoice.fiscal_operation_category_id.id == fopc_id
     assert invoice.fiscal_operation_id.id == fop_id
 
     #confirm invoice
-    wf_service.trg_validate(1, 'account.invoice', inv_id, 'invoice_sefaz_export', oerp.cr)
+    wf_service.trg_validate(1, 'account.invoice', inv_id, 'invoice_validate', oerp.cr)
 
     # reload invoice to get new fields created by the action on the workflow
     invoice = inv_obj.browse(oerp.cr, 1, [inv_id])[0]
 
     move_line_obj = oerp.pool.get('account.move.line')
     move_lines = move_line_obj.search(oerp.cr, 1, [('move_id', '=', invoice.move_id.id)])
-    assert len(move_lines) == 7
+    assert len(move_lines) == 8
 
     move1 = move_line_obj.search(oerp.cr, 1, [('move_id', '=', invoice.move_id.id), ('debit', '=', 105)])[0]
     assert move_line_obj.browse(oerp.cr, 1, move1).debit == 105
@@ -417,17 +550,20 @@ def test_product_sale_to_invoice_nfe(oerp):
     move3 = move_line_obj.search(oerp.cr, 1, [('move_id', '=', invoice.move_id.id), ('credit', '=', 0.74)])[0]
     assert move_line_obj.browse(oerp.cr, 1, move3).credit == 0.74
 
-    move4 = move_line_obj.search(oerp.cr, 1, [('move_id', '=', invoice.move_id.id), ('credit', '=', 1.80)])[0]
-    assert move_line_obj.browse(oerp.cr, 1, move4).credit == 1.80
+    move4 = move_line_obj.search(oerp.cr, 1, [('move_id', '=', invoice.move_id.id), ('credit', '=', 0.25)])[0]
+    assert move_line_obj.browse(oerp.cr, 1, move4).credit == 0.25
 
-    move5 = move_line_obj.search(oerp.cr, 1, [('move_id', '=', invoice.move_id.id), ('credit', '=', 1.25)])[0]
-    assert move_line_obj.browse(oerp.cr, 1, move5).credit == 1.25
+    move5 = move_line_obj.search(oerp.cr, 1, [('move_id', '=', invoice.move_id.id), ('credit', '=', 1.80)])[0]
+    assert move_line_obj.browse(oerp.cr, 1, move5).credit == 1.80
 
-    move6 = move_line_obj.search(oerp.cr, 1, [('move_id', '=', invoice.move_id.id), ('credit', '=', 5.00)])[0]
-    assert move_line_obj.browse(oerp.cr, 1, move6).credit == 5
+    move6 = move_line_obj.search(oerp.cr, 1, [('move_id', '=', invoice.move_id.id), ('credit', '=', 1.25)])[0]
+    assert move_line_obj.browse(oerp.cr, 1, move6).credit == 1.25
 
-    move7 = move_line_obj.search(oerp.cr, 1, [('move_id', '=', invoice.move_id.id), ('credit', '=', 96)])[0]
-    assert move_line_obj.browse(oerp.cr, 1, move7).credit == 96
+    move7 = move_line_obj.search(oerp.cr, 1, [('move_id', '=', invoice.move_id.id), ('credit', '=', 5.00)])[0]
+    assert move_line_obj.browse(oerp.cr, 1, move7).credit == 5
+
+    move8 = move_line_obj.search(oerp.cr, 1, [('move_id', '=', invoice.move_id.id), ('credit', '=', 95.75)])[0]
+    assert move_line_obj.browse(oerp.cr, 1, move8).credit == 95.75
 
     rec_ids = invoice._get_receivable_lines(inv_id, False, None)
     assert len(rec_ids) == 1

6c506633fe81cdea06f516dc799d34d722447edd
Daniel Hartmann <daniel@proge.com.br>
2013-03-15 14:17:52 -0300

    Migrated l10n_br_data_base to 7.0

--- l10n_br_data_base/__openerp__.py
+++ l10n_br_data_base/__openerp__.py
@@ -1,41 +1,35 @@
-# -*- encoding: utf-8 -*-
-#################################################################################
-#                                                                               #
-# Copyright (C) 2009  Renato Lima - Akretion                                    #
-#                                                                               #
-#This program is free software: you can redistribute it and/or modify           #
-#it under the terms of the GNU Affero General Public License as published by    #
-#the Free Software Foundation, either version 3 of the License, or              #
-#(at your option) any later version.                                            #
-#                                                                               #
-#This program is distributed in the hope that it will be useful,                #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-#GNU Affero General Public License for more details.                            #
-#                                                                               #
-#You should have received a copy of the GNU Affero General Public License       #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
-#################################################################################
+# -*- coding: utf-8 -*-
+###############################################################################
+#                                                                             #
+# Copyright (C) 2009  Renato Lima - Akretion                                  #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU Affero General Public License as published by #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU Affero General Public License for more details.                         #
+#                                                                             #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+###############################################################################
 
 {
-    "name" : "Brazilian Localisation Data Extension for Base",
-    "description" : "Brazilian Localisation Data Extension for Base",
+    "name": "Brazilian Localization Data Extension for Base",
+    "description": "Brazilian Localization Data Extension for Base",
     'license': 'AGPL-3',
-    "author" : "Akretion, OpenERP Brasil",
-    "version" : "0.1",
-    "depends" : [
-                 "l10n_br_base"
-                 ],
-    'init_xml': [
-                'res.bank.csv',
-                'l10n_br_base_data.xml',
-                ],
-    "update_xml" : [
-
-                   ],
-    "category" : "Localisation",
-    "active": False,
-    "installable": True
+    "author": "Akretion, OpenERP Brasil",
+    "version": "0.1",
+    "depends": [
+        "l10n_br_base"
+        ],
+    'data': [
+        'res.bank.csv',
+        'l10n_br_base_data.xml',
+        ],
+    "category": "Localization",
+    "installable": True,
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
